51eab1d83a80a09fd84d47b5e1c91d5bbdaed1bc
samanyuGY <86158217+samanyuGY@users.noreply.github.com>
2023-06-12 20:30:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+status = "";
+
+function setup() {
+    canvas = createCanvas(480,380);
+    canvas.center()
+
+    video = createCapture(VIDEO)
+    video.hide()
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+}
+
+function draw() {
+    image(video, 0, 0, 480, 380);
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
